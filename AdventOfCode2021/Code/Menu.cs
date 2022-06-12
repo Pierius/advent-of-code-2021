533f88464e2a2b6e63c381501075f37f2eca81e0
Pierius Lan <pierius_lan@hotmail.com>
2022-06-12 14:39:33 +0200

    Setting up inner workings

---
+++ AdventOfCode2021/Code/Menu.cs
@@ -0,0 +1,72 @@
+﻿using AdventOfCode2021.Code.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Code
+{
+    public class Menu
+    {
+        private const int MINIMUM_DAY = 1;
+        private const int MAXIMUM_DAY = 25;
+
+        public void OpenMenu()
+        {
+            string errorMessage = string.Empty;
+
+            while (true)
+            {
+                Console.Clear();
+
+                if (errorMessage != string.Empty)
+                {
+                    Console.WriteLine($"Error: {errorMessage}\n");
+                    errorMessage = string.Empty;
+                }
+
+                Console.WriteLine("Welcome to Advent of Code!");
+                Console.Write($"Choose your day ({MINIMUM_DAY}-{MAXIMUM_DAY}):");
+
+                string? userInput = Console.ReadLine();
+
+                if (userInput != null && 
+                    (userInput.ToLower() == "q" || userInput.ToLower() == "quit"))
+                {
+                    break; // Quit program
+                }
+
+                Console.WriteLine(); // Line break                
+                
+                Day day = ConvertToDay(userInput);
+                
+                if (day == Day.Invalid)
+                {
+                    errorMessage = $"Only days between {MINIMUM_DAY} and {MAXIMUM_DAY} are valid!";
+                    continue;
+                }
+
+                Console.WriteLine($"Executing Day: {day}");
+                new Puzzle().Execute(day);
+                
+                Console.ReadKey();
+            }
+        }
+
+        private Day ConvertToDay(string? value)
+        {
+            if (string.IsNullOrWhiteSpace(value) || !Int32.TryParse(value, out int day))
+            {
+                return Day.Invalid;
+            }
+
+            if (day < MINIMUM_DAY || day > MAXIMUM_DAY)
+            {
+                return Day.Invalid;
+            }
+
+            return (Day)day;
+        }
+    }
+}
---
+++ AdventOfCode2021/Code/Puzzle.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode2021.Code.Days;
+using AdventOfCode2021.Code.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Code
+{
+    internal class Puzzle
+    {
+        public void Execute(Day day)
+        {
+            switch (day)
+            {
+                case Day.One:
+                    new DayOne().Execute();
+                    break;
+            }
+        }
+    }
+}
